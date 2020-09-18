accf1b5e583af5da5253c8ccdbd793bb9270f130
urawa72 <urawa72@gmail.com>
2020-09-18 22:14:24 +0900

    add aws cli sample

---
+++ src/aws_cli_sample/go.mod
@@ -0,0 +1,8 @@
+module awsclisample
+
+go 1.15
+
+require (
+	github.com/aws/aws-sdk-go v1.34.24
+	gopkg.in/AlecAivazis/survey.v1 v1.8.8
+)
---
+++ src/aws_cli_sample/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+    "gopkg.in/AlecAivazis/survey.v1"
+)
+
+func main() {
+    // sess := session.Must(session.NewSessionWithOptions(session.Options{
+    //     Profile: "kobayashi.yuki",
+    //     SharedConfigState: session.SharedConfigEnable,
+    // }))
+    sess := session.Must(session.NewSession())
+
+    svc := dynamodb.New(
+        sess,
+        aws.NewConfig().WithRegion("ap-northeast-1"),
+    )
+
+    input := &dynamodb.ListTablesInput{}
+    result, err := svc.ListTables(input)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(result)
+
+    color := ""
+    prompt := &survey.Select{
+        Message: "Choose a color:",
+        Options: []string{"red", "blue", "green"},
+    }
+    survey.AskOne(prompt, &color, nil)
+
+    fmt.Println(color)
+
+}
